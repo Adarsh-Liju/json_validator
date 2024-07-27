686b8955f38dc83e2d6f5d23d226a415e11c75fe
Adarsh Liju <adarsh.abraham@caliperbusiness.com>
2024-07-27 19:48:43 +0530

    templates and bug fixes

--- json_validator_app/views.py
+++ json_validator_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+class MainScreen(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        template = loader.get_template('index.html')
+        return HttpResponse(template.render())
