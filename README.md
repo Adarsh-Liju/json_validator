9790af47b29bd54df2141454060ef0053fe7c4d1
Adarsh Liju <adarsh.abraham@caliperbusiness.com>
2024-07-27 19:28:11 +0530

    Genesis Code

--- README.md
+++
@@ -1,2 +0,0 @@
-# json-validator
-json-validator
---
+++ json_validator_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JsonValidatorAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'json_validator_app'
